6d8f1fcf8246c2d840fa0e0025588809258e665b
nechiyan3 <navasnechiyan3@gmail.com>
2024-08-27 22:45:38 +0530

    python explained code

---
+++ sample.py
@@ -0,0 +1,17 @@
+s = "aabbcc"
+
+#The function f(s) analyzes the count of characters in a given string and 
+# returns a dictionary with each unique character as a key and its count as the value.
+#suppose s is the string
+def f(s):
+    r = {}  # Create an empty dictionary 'r' to store the character counts
+    for i in s: # Loop through each character in the string 's'
+        if i in r:  # Check if the character is already in the dictionary 'r'
+            r[i] += 1  # Increment the count if found.
+        else:
+            r[i] = 1  # else Add the character to the dictionary with an initial count of 1 .
+    return r # After processing all characters, return the dictionary 'r'
+
+
+result = f(s)
+print(result)
